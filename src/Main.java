0468c8912b7667767684ea966291cda94d8359f6
nikita0106 <valinnikita@gmail.com>
2021-09-11 19:24:48 +0300

    Update

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,20 @@
 import java.security.SecureRandom;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.Scanner;
-
-
+import java.util.Set;
 
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
+
+        Set<String> set = new HashSet<String>();
+        Collections.addAll(set, args);
+        if (set.size() != args.length) {
+            System.out.println("Invalid parameter");
+            System.exit(0);
+        }
 
         if (args.length%2 == 0 || args.length <= 1) {
             System.out.println("Invalid parameter");
